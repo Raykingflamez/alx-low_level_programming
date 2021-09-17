fbfeb2003a18a40279dbab49350ab7e2b5dd00cc
Raykingflamez <raykingflamez@gmail.com>
2021-09-17 13:34:18 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
